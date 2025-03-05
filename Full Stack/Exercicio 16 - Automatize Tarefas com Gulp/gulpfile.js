fff66016c8d5624a9936dc5fc908cc2eb3822497
AndradeTC86 <andrade-tc@uol.com.br>
2025-03-05 19:18:13 -0300

    Compilando SASS com Gulp

--- Full Stack/Exercicio 16 - Automatize Tarefas com Gulp/gulpfile.js
+++ Full Stack/Exercicio 16 - Automatize Tarefas com Gulp/gulpfile.js
@@ -1,23 +1,19 @@
 const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps')
 
-function funcaoPadrao(callback) {
-    setTimeout(() => {        
-        console.log('Executando via Gulp')
-        callback()
-    }, 2000)
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'))
 }
 
-function dizOi(callback) {
-    setTimeout(() => {
-        console.log('Oi')
-        dizTchau()
-        callback()
-    }, 3000)
-}
+exports.default = compilaSass
 
-function dizTchau() {
-    console.log('Tchau') 
+exports.watch = () => {
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass))
 }
-
-exports.parallel = gulp.parallel(funcaoPadrao, dizOi)
-exports.series = gulp.series(funcaoPadrao, dizOi)
