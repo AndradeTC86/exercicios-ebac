0037abcc918a884a8208468f387e25ca6f593893
AndradeTC86 <andrade-tc@uol.com.br>
2025-03-05 18:36:35 -0300

    Automatizando com Gulp

--- Full Stack/Exercicio 16 - Automatize Tarefas com Gulp/gulpfile.js
+++ Full Stack/Exercicio 16 - Automatize Tarefas com Gulp/gulpfile.js
@@ -1,6 +1,18 @@
+const gulp = require('gulp')
+
 function funcaoPadrao(callback) {
     console.log('Executando via Gulp')
     callback()
 }
 
-exports.default = funcaoPadrao
+function dizOi(callback) {
+    console.log('Oi')
+    dizTchau()
+    callback()
+}
+
+function dizTchau() {
+    console.log('Tchau') 
+}
+
+exports.default = gulp.series(funcaoPadrao, dizOi)
