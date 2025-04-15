e84da3bd93593340064037a4f5ae9b565d5741c0
AndradeTC86 <andrade-tc@uol.com.br>
2025-04-15 19:50:21 -0300

    Instanciando objetos

--- FullStack/Exercicio24/main.js
+++ FullStack/Exercicio24/main.js
@@ -14,3 +14,26 @@ function Carro(modelo, fabricante, anoModelo, anoFabricacao, cor) {
 
 const carroJoao = new Carro('Fusca', 'Volkswagen', 1970, 1969, 'azul')
 const carroMaria = new Carro('Civic', 'Honda', 2020, 2019, 'preto')
+
+console.log(carroJoao)
+console.log(carroMaria) 
+
+const nome = 'João'
+const idade = 30
+const maiorDeIdade = true
+const conhecimentos = ['JavaScript', 'HTML', 'CSS']
+
+const pessoa = {
+    nome: nome,
+    idade: idade,
+    maiorDeIdade: maiorDeIdade,
+    conhecimentos: conhecimentos,
+}
+
+console.log( typeof nome)
+console.log( typeof idade)
+console.log( typeof maiorDeIdade)
+console.log( typeof conhecimentos)
+console.log(carroMaria instanceof Carro)
+console.log(carroJoao instanceof Carro)
+console.log(conhecimentos instanceof Array)
