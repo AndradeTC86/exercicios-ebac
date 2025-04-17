fb89e4ffca60560e2f23dc3f965a3cafb3d8aff6
AndradeTC86 <andrade-tc@uol.com.br>
2025-04-17 19:47:33 -0300

    Aplicando polimorfismo

--- FullStack/Exercicio24/main.js
+++ FullStack/Exercicio24/main.js
@@ -1,40 +1,63 @@
-function Carro(modelo, fabricante, anoModelo, anoFabricacao, cor) {
-    this.modelo = modelo
-    this.fabricante = fabricante
-    this.anoModelo = anoModelo
-    this.anoFabricacao = anoFabricacao
-    this.cor = cor
-    this.acelerar = function() {
-        console.log('Acelerando...')
+function Pessoa(nome) {
+    this.nome = nome
+    this.falar = function() {
+        console.log('Olá, meu nome é ' + this.nome)
+    }        
+}
+
+function Funcionario(nome, cargo, salario) {    
+    this.cargo = cargo
+    let _salario = salario
+    this.falarCargo = function() {
+        console.log('eu sou ' + this.cargo)
     }
-    this.frear = function() {
-        console.log('Freando...')
+
+    Pessoa.call(this, nome)
+
+    this.getSalario = function() {
+        return _salario
+    }
+
+    this.setSalario = function(valor) {
+       if (typeof valor === 'number') {
+            _salario = valor
+        }
+        else {
+            console.log('Valor inválido')
+        }
+    }
+
+    this.setPromocao = function() {
+        const novoSalario = _salario * 1.1
+        _salario = novoSalario        
     }
 }
 
-const carroJoao = new Carro('Fusca', 'Volkswagen', 1970, 1969, 'azul')
-const carroMaria = new Carro('Civic', 'Honda', 2020, 2019, 'preto')
+function Estagiario(nome) {
+    Funcionario.call(this, nome, 'Estagiário', 2000)
 
-const nome = 'João'
-const idade = 30
-const maiorDeIdade = true
-const conhecimentos = ['JavaScript', 'HTML', 'CSS']
-
-const pessoa = {
-    nome: nome,
-    idade: idade,
-    maiorDeIdade: maiorDeIdade,
-    conhecimentos: conhecimentos,
+    this.setPromocao = function() {
+        const novoSalario = this.getSalario() * 1.07
+        this.setSalario(novoSalario)
+    }
 }
 
-function exibirDados(dado) {
-    console.log(pessoa[dado])
+function Gerente(nome) {
+    Funcionario.call(this, nome, 'Gerente', 10000)
+
+    this.setPromocao = function() {
+        const novoSalario = this.getSalario() * 1.15
+        this.setSalario(novoSalario)
+    }
 }
 
-exibirDados('nome')
-exibirDados('idade')
-exibirDados('maiorDeIdade')
-exibirDados('conhecimentos')
+const funcionario1 = new Funcionario('Maria', 'Desenvolvedora', 5000)
+const funcionario2 = new Estagiario('João')
+const funcionario3 = new Gerente('Carlos')
 
-console.log(Object.keys(pessoa))
-console.log(Object.values(pessoa))
+funcionario1.setPromocao()
+console.log(funcionario1.getSalario())
+funcionario2.setPromocao()
+console.log(funcionario2.getSalario())
+funcionario3.setPromocao()
+console.log(funcionario3.getSalario())
