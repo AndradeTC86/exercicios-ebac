7771249526e590a816ce6bf85492a3a7cbcc0294
AndradeTC86 <andrade-tc@uol.com.br>
2025-04-17 18:42:24 -0300

    Acessando atributos

--- FullStack/Exercicio24/main.js
+++ FullStack/Exercicio24/main.js
@@ -15,9 +15,6 @@ function Carro(modelo, fabricante, anoModelo, anoFabricacao, cor) {
 const carroJoao = new Carro('Fusca', 'Volkswagen', 1970, 1969, 'azul')
 const carroMaria = new Carro('Civic', 'Honda', 2020, 2019, 'preto')
 
-console.log(carroJoao)
-console.log(carroMaria) 
-
 const nome = 'João'
 const idade = 30
 const maiorDeIdade = true
@@ -30,10 +27,14 @@ const pessoa = {
     conhecimentos: conhecimentos,
 }
 
-console.log( typeof nome)
-console.log( typeof idade)
-console.log( typeof maiorDeIdade)
-console.log( typeof conhecimentos)
-console.log(carroMaria instanceof Carro)
-console.log(carroJoao instanceof Carro)
-console.log(conhecimentos instanceof Array)
+function exibirDados(dado) {
+    console.log(pessoa[dado])
+}
+
+exibirDados('nome')
+exibirDados('idade')
+exibirDados('maiorDeIdade')
+exibirDados('conhecimentos')
+
+console.log(Object.keys(pessoa))
+console.log(Object.values(pessoa))
