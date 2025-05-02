346b2eb298c5f9f26f7366f294189ed92a95f575
AndradeTC86 <andrade-tc@uol.com.br>
2025-05-02 15:59:35 -0300

    Map e Set

---
+++ FrontEnd/Exercicio25/dist/map_set.js
@@ -0,0 +1,56 @@
+"use strict";
+
+function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t.return && (u = t.return(), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
+function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
+function _toConsumableArray(r) { return _arrayWithoutHoles(r) || _iterableToArray(r) || _unsupportedIterableToArray(r) || _nonIterableSpread(); }
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _iterableToArray(r) { if ("undefined" != typeof Symbol && null != r[Symbol.iterator] || null != r["@@iterator"]) return Array.from(r); }
+function _arrayWithoutHoles(r) { if (Array.isArray(r)) return _arrayLikeToArray(r); }
+function _createForOfIteratorHelper(r, e) { var t = "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (!t) { if (Array.isArray(r) || (t = _unsupportedIterableToArray(r)) || e && r && "number" == typeof r.length) { t && (r = t); var _n = 0, F = function F() {}; return { s: F, n: function n() { return _n >= r.length ? { done: !0 } : { done: !1, value: r[_n++] }; }, e: function e(r) { throw r; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var o, a = !0, u = !1; return { s: function s() { t = t.call(r); }, n: function n() { var r = t.next(); return a = r.done, r; }, e: function e(r) { u = !0, o = r; }, f: function f() { try { a || null == t.return || t.return(); } finally { if (u) throw o; } } }; }
+function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
+function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
+//Map
+var pessoa = new Map();
+pessoa.set('nome', 'Lucas');
+pessoa.set('idade', 25);
+pessoa.set('profissao', 'programador');
+pessoa.set('stack', 'html, css, js');
+console.log(pessoa.get('nome'));
+console.log(pessoa.size);
+console.log(pessoa.has('stack'));
+var _iterator = _createForOfIteratorHelper(pessoa.entries()),
+  _step;
+try {
+  for (_iterator.s(); !(_step = _iterator.n()).done;) {
+    var _step$value = _slicedToArray(_step.value, 2),
+      chave = _step$value[0],
+      valor = _step$value[1];
+    console.log("Chave: ".concat(chave, ", Valor: ").concat(valor));
+  }
+} catch (err) {
+  _iterator.e(err);
+} finally {
+  _iterator.f();
+}
+pessoa.delete('idade');
+console.log(pessoa.size);
+
+//Set
+
+var cpfs = new Set();
+cpfs.add('21210523051');
+cpfs.add('54767232007');
+cpfs.add('91996236091');
+cpfs.add('40445726075');
+cpfs.add('64894015099');
+cpfs.add('71580264000');
+cpfs.forEach(function (cpf) {
+  console.log(cpf);
+});
+var arrayNomes = ['João', 'Lucas', 'Ana', 'Maria', 'Pedro', 'Lucas'];
+var arraySet = new Set([].concat(arrayNomes));
+var arraySemDuplicados = _toConsumableArray(arraySet);
+console.log(arraySet);
+console.log(arraySemDuplicados);
