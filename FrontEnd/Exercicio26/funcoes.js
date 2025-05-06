9ee9ff471485841028996c2c77b61170dee70dc0
AndradeTC86 <andrade-tc@uol.com.br>
2025-05-06 18:22:44 -0300

    Aplicando tipos nas funcoes

---
+++ FrontEnd/Exercicio26/funcoes.js
@@ -0,0 +1,17 @@
+"use strict";
+function calcularArea(base, altura) {
+    return base * altura;
+}
+const calcularAreaArrow = (base, altura) => base * altura;
+function somar(...numeros) {
+    console.log(numeros);
+}
+function teste() {
+    if (10 > 5) {
+        return 'Dez é maior que cinco';
+    }
+    else {
+        return 5;
+    }
+}
+const resultadoDeTeste = teste();
