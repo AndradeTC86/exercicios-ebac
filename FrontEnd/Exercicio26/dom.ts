7b7a43d5b6df59c54d75733fb1240bc78aacde5c
AndradeTC86 <andrade-tc@uol.com.br>
2025-05-07 07:12:44 -0300

    Entregando exercicio

--- FrontEnd/Exercicio26/dom.ts
+++
@@ -1,9 +0,0 @@
-const campoNome = document.getElementById("nome")
-
-const formulario = document.getElementsByTagName("form")[0]
-
-const botaoEnviar = document.getElementById("enviar")
-
-botaoEnviar?.addEventListener('click', (evento: MouseEvent) => {
-    evento.preventDefault()
-})
